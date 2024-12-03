ef795b7684565782295dcd2efa5c1ebc115fbf21
Larissa Cerqueira <148367337+larissafcerqueira@users.noreply.github.com>
2024-12-03 14:49:39 -0300

    Update Day_3.sql

--- Day3/Day_3.sql
+++ Day3/Day_3.sql
@@ -1,3 +1,3 @@
-SELECT candy_name,calories,candy_category,
+SELECT candy_name, candy_category, calories,
 RANK() OVER (PARTITION BY candy_category ORDER BY calories DESC) AS rank_in_category
 FROM candy_nutrition;
